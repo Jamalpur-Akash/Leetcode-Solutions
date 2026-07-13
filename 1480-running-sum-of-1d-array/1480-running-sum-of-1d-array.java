d47793141d3275582b4418bbd18f20a11b89c394
Jamalpur-Akash <akashjamalpur9@gmail.com>
2026-07-13 23:32:19 +0530

    Time: 0 ms (100%), Space: 44.1 MB (72.23%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+            if(i-1<0){
+                nums[i]=0+nums[i];
+            } else{
+            nums[i]=nums[i-1]+nums[i];
+            }
+        }
+        return nums;
+    }
+}
