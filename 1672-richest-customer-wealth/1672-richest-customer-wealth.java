be93846e14af0d557d3f749e63da6a799e0c1da0
Jamalpur Akash <akashjamalpur9@gmail.com>
2026-07-20 22:28:54 +0530

    Time: 0 ms (100%), Space: 44.5 MB (56.03%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int sum=0;
+        int maxSum=0;
+        for(int i=0;i<accounts.length;i++){
+            for(int j=0;j<accounts[i].length;j++){
+                sum+=accounts[i][j];
+            }
+
+            if(maxSum<=sum || maxSum==0){
+                maxSum=sum;
+            }
+
+            sum=0;
+        }
+        return maxSum;
+    }
+}
