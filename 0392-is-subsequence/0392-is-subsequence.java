76559ad1bf0fba91b4e1dd9ab91197bf279f0a02
Jamalpur Akash <akashjamalpur9@gmail.com>
2026-08-10 15:41:19 +0530

    Time: 2 ms (73.89%), Space: 43 MB (11.36%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int p=0;
+        int q=0;
+        while(p<s.length() && q<t.length()){
+            if(s.charAt(p)==t.charAt(q)){
+                p++;
+            }
+            q++;
+        }
+        return p==s.length();
+    }
+}
