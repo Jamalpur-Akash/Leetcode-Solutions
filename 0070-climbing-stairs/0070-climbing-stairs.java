89688f5152544f34d8d0375722266b78e7969032
Jamalpur Akash <akashjamalpur9@gmail.com>
2026-08-15 23:14:57 +0530

    Time: 0 ms (100%), Space: 41.8 MB (94.5%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n<=2){
+            return n;
+        }
+
+        int ఒకటి = 1;
+        int రెండు = 2;
+        for(int i=3;i<=n;i++){
+            int మూడు = ఒకటి + రెండు;
+            ఒకటి = రెండు;
+            రెండు = మూడు;
+        }
+        return రెండు;
+    }
+}
