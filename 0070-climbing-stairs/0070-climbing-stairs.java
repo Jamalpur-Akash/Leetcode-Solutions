ba369758d8df4dee818689ca381c94b04400d29f
Jamalpur Akash <akashjamalpur9@gmail.com>
2026-08-15 23:16:06 +0530

    Time: 0 ms (100%), Space: 42.1 MB (55.27%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -3,7 +3,8 @@ class Solution {
         if(n<=2){
             return n;
         }
-
+        
+        //JAVA SUPPORTS UNICODE CHARACTERS
         int ఒకటి = 1;
         int రెండు = 2;
         for(int i=3;i<=n;i++){
