bcf0c42de9fc86bb93e58d0e3d59c9e48bcc83f6
Jamalpur Akash <akashjamalpur9@gmail.com>
2026-09-04 23:58:22 +0530

    Time: 1 ms (99.79%), Space: 46.4 MB (32.13%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
@@ -2,46 +2,34 @@ class Solution {
     public int firstStableIndex(int[] nums, int k) {
         
         
-        int leastStable=Integer.MAX_VALUE;
-        int stable=-1;
-        for(int i=0;i<nums.length;i++){
-        int max=Integer.MIN_VALUE;
-        int min=Integer.MAX_VALUE;
-    
-        for(int j=0;j<=i;j++){
-            if(i==0){
-                max=nums[i];
-                break;
-            }
-            if(max<nums[j]){
-                max=nums[j];
-            }
-        }
-
-        for(int j=i;j<nums.length;j++){
-            if(i==nums.length-1){
-                min=nums[i];
-            }
-            if(min>nums[j]){
-                min=nums[j];
-            }
-
-        }
+        // 
         
+        int n = nums.length;
 
-        int score=max-min;
-        if(score<=k){
-            stable=i;
-        
-            if(stable<leastStable){
-                leastStable=stable;
+        int[] prefixMax = new int[n];
+        int[] suffixMin = new int[n];
+
+        prefixMax[0] = nums[0];
+
+        for(int i = 1; i < n; i++) {
+            prefixMax[i] = Math.max(prefixMax[i - 1], nums[i]);
+        }
+
+        suffixMin[n - 1] = nums[n - 1];
+
+        for(int i = n - 2; i >= 0; i--) {
+            suffixMin[i] = Math.min(suffixMin[i + 1], nums[i]);
+        }
+
+        for(int i = 0; i < n; i++) {
+            int score = prefixMax[i] - suffixMin[i];
+
+            if(score <= k) {
+                return i;
             }
         }
-        }
-        if(stable==-1){
-            return -1;
-        }
-        return leastStable;
+
+        return -1;
         
 
     }
